9faf4e16b62411d3c380576bf75ac8eab14e1a89
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-04-05 15:51:19 +0300

    feat: create all components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from "react";
+
 import "./App.css";
+import Header from "./components/header/header.component";
+import ItemList from "./components/item-list/item-list.component";
+import PersonDetails from "./components/person-details/person-details.component";
+import RandomPlanet from "./components/random-planet/random-planet.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+    <div>
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/random-planet/random-planet.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RandomPlanet = () => {
+  return <div></div>;
+};
+
+export default RandomPlanet;

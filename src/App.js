587a78330c459166b479ca1d25d36a0924b90ad3
Vasily <v.ilyuhin@gmail.com>
2022-04-09 09:18:55 +0300

    feat: add render list of persons

--- src/App.js
+++ src/App.js
@@ -6,22 +6,52 @@ import ItemList from "./components/item-list/item-list.component";
 import PersonDetails from "./components/person-details/person-details.component";
 import RandomPlanet from "./components/random-planet/random-planet.component";
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-      <RandomPlanet />
+class App extends React.Component {
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: null,
+  };
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet,
+      };
+    });
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    });
+  };
+
+  render() {
+    const { showRandomPlanet } = this.state;
+
+    return (
+      <div className="container">
+        <Header />
+        {showRandomPlanet && <RandomPlanet />}
+
+        <button
+          className="toggle-planet btn btn-warning btn-lg mb-2"
+          onClick={this.toggleRandomPlanet}
+        >
+          Toggle Random Planet
+        </button>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected} />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/item-list/item-list.component.jsx
+++ src/components/item-list/item-list.component.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 
 import "./item-list.styles.css";
+import SwapiService from "./../../services/swapi-service";
+import Spiner from "./../spiner/spiner.component";
 
 export default class ItemList extends React.Component {
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: [],
+  };
+
+  componentDidMount() {
+    this.swapiService.getAllPeople().then((peopleList) => {
+      this.setState({ peopleList: peopleList });
+    });
+  }
+
+  renderItems(arr) {
+    return arr.map(({ id, name }) => {
+      return (
+        <li
+          className="list-group-item"
+          key={id}
+          onClick={() => this.props.onItemSelected(id)}
+        >
+          {name}
+        </li>
+      );
+    });
+  }
   render() {
-    return (
-      <ul className="item-list list-group">
-        <li className="list-group-item">Luke Skywalker</li>
-        <li className="list-group-item">Darth Vader</li>
-        <li className="list-group-item">R2-D2</li>
-      </ul>
-    );
+    const { peopleList } = this.state;
+
+    if (peopleList.length < 1) {
+      return <Spiner />;
+    } else {
+      const list = this.renderItems(peopleList);
+      return <ul className="item-list list-group">{list}</ul>;
+    }
   }
 }
